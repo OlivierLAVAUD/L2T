a33def75b9e4dc7542ca7b8854636736df387cf3
OLivier LAVAUD <olivier.lavaud@gmail.com>
2025-05-18 14:50:20 +0200

    Update l2t-alias.ps1

--- l2t-alias.ps1
+++ l2t-alias.ps1
@@ -1,10 +1,15 @@
 function Invoke-L2T {
-    param(
-        [string]$Text,
-        [string]$Language = "fra_Latn"  # Valeur par défaut
-    )
-    uv run -m app.interfaces.main $Text -t $Language
+    uv run -m app.main @args
 }
 
-# Associer l'alias à la fonction
+# Supprime un alias existant s’il y en a un
+if (Get-Alias l2t -ErrorAction SilentlyContinue) {
+    Remove-Item Alias:l2t
+}
+
+. $PROFILE
+
+# Crée l’alias
 Set-Alias -Name l2t -Value Invoke-L2T
+
+. $PROFILE
